a0c544fd5e99722109457f9c27390dc779bf6138
funnyicecream <mak3.youself@gmail.com>
2023-09-02 10:50:45 +0300

    - Home: finished tab 'new' for unauthorised users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,14 +22,22 @@ export const Home = () => {
     };
 
     const [events, setEvents] = useState([]);
-    let postsLoading = false;
+    const [newEvents, setNewEvents] = useState([]);
+    //let postsLoading = false;
 
     React.useEffect(() => {
         axios.get('/event/')
             .then((response) => {
                 console.log(response.data);
                 setEvents(response.data);
-                postsLoading = true;
+                //postsLoading = true;
+            })
+
+        axios.get('/event/new', { params: { Limit: 10 } })
+            .then((response) => {
+                console.log(response.data);
+                setNewEvents(response.data);
+                //postsLoading = true;
             })
     }, []);
 
@@ -43,9 +51,9 @@ export const Home = () => {
                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                         <TabList onChange={handleChange} aria-label="lab API tabs example">
                             <Tab label="Все" value="1" />
-                            <Tab label="Только мои" value="2" />
-                            <Tab label="Популярные" value="3" />
-                            <Tab label="Новые" value="4" />
+                            <Tab label="Новые" value="2" />
+                            <Tab label="Только мои" value="3" />
+                            <Tab label="Популярные" value="4" />
                         </TabList>
                     </Box>
 
@@ -67,7 +75,7 @@ export const Home = () => {
                                         commentsCount={332}
                                         tags={[obj.categoryName]}
                                         isEditable
-                                        isLoading={postsLoading === 'loaded'}
+                                        //isLoading={postsLoading === 'loaded'}
                                     />
                                 ))}
 
@@ -100,21 +108,69 @@ export const Home = () => {
                         </Grid>
                     </TabPanel>
                     <TabPanel value="2">
-                        Here will be your own events soon...
+                        <Grid container spacing={4}>
+                            <Grid xs={8} item>
+                                {newEvents.map((obj, index) => (
+                                    <Post
+                                        id={index}
+                                        title={obj.description}
+                                        book={obj.bookAuthor + ": " + obj.bookTitle}
+                                        imageUrl="https://img.freepik.com/premium-vector/people-reading-park_73637-401.jpg?w=2000"
+                                        user={{
+                                            avatarUrl: obj.avatar,
+                                            fullName: obj.creatorName
+                                        }}
+                                        createdAt={obj.createdAt}
+                                        viewsCount={154}
+                                        commentsCount={12}
+                                        tags={[obj.categoryName]}
+                                        isEditable
+                                        //isLoading={postsLoading === 'loaded'}
+                                    />
+                                ))}
+                            </Grid>
+
+                            <Grid xs={4} item>
+                                <TagsBlock
+                                    items={["bestseller", "fun", "historic"]}
+                                    isLoading={false}
+                                />
+                                <CommentsBlock
+                                    items={[
+                                        {
+                                            user: {
+                                                fullName: "Василий Малов",
+                                                avatarUrl: "https://img.freepik.com/premium-psd/people-avatar-3d-illustration_235528-1573.jpg?w=2000",
+                                            },
+                                            text: "Тест комментария тут",
+                                        },
+                                        {
+                                            user: {
+                                                fullName: "Иван Доставалов",
+                                                avatarUrl: "https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436189.jpg?w=2000&t=st=1693334811~exp=1693335411~hmac=b139cdad69fa6b60313326e17037575dd12df8a0a589a555dc200b1fbeed35cf",
+                                            },
+                                            text: "Тест комментария тут. Тест комментария тут. Тест комментария тут",
+                                        },
+                                    ]}
+                                    isLoading={false}
+                                />
+                            </Grid>
+                        </Grid>
                     </TabPanel>
                     <TabPanel value="3">
-                        Here will be the most popular events soon...
+                        Here will be your own events soon...
                     </TabPanel>
                     <TabPanel value="4">
-                        Here will be new events soon...
+                        Here will be the most popular events soon...
                     </TabPanel>
                 </TabContext>
             ) : (
-                // show only first tab
+                // show tab: all & new
                 <TabContext value={value}>
                     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                         <TabList onChange={handleChange} aria-label="lab API tabs example">
                             <Tab label="Все" value="1" />
+                            <Tab label="Новые" value="2" />
                         </TabList>
                     </Box>
                     <TabPanel value="1">
@@ -135,11 +191,61 @@ export const Home = () => {
                                         commentsCount={12}
                                         tags={[obj.categoryName]}
                                         isEditable
-                                        isLoading={postsLoading === 'loaded'}
+                                        //isLoading={postsLoading === 'loaded'}
                                     />
                                 ))}
 
                             </Grid>
+                            <Grid xs={4} item>
+                                <TagsBlock
+                                    items={["bestseller", "fun", "historic"]}
+                                    isLoading={false}
+                                />
+                                <CommentsBlock
+                                    items={[
+                                        {
+                                            user: {
+                                                fullName: "Василий Малов",
+                                                avatarUrl: "https://img.freepik.com/premium-psd/people-avatar-3d-illustration_235528-1573.jpg?w=2000",
+                                            },
+                                            text: "Тест комментария тут",
+                                        },
+                                        {
+                                            user: {
+                                                fullName: "Иван Доставалов",
+                                                avatarUrl: "https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436189.jpg?w=2000&t=st=1693334811~exp=1693335411~hmac=b139cdad69fa6b60313326e17037575dd12df8a0a589a555dc200b1fbeed35cf",
+                                            },
+                                            text: "Тест комментария тут. Тест комментария тут. Тест комментария тут",
+                                        },
+                                    ]}
+                                    isLoading={false}
+                                />
+                            </Grid>
+                        </Grid>
+                    </TabPanel>
+                    <TabPanel value="2">
+                        <Grid container spacing={4}>
+                            <Grid xs={8} item>
+                                {newEvents.map((obj, index) => (
+                                    <Post
+                                        id={index}
+                                        title={obj.description}
+                                        book={obj.bookAuthor + ": " + obj.bookTitle}
+                                        imageUrl="https://img.freepik.com/premium-vector/people-reading-park_73637-401.jpg?w=2000"
+                                        user={{
+                                            avatarUrl: obj.avatar,
+                                            fullName: obj.creatorName
+                                        }}
+                                        createdAt={obj.createdAt}
+                                        viewsCount={154}
+                                        commentsCount={12}
+                                        tags={[obj.categoryName]}
+                                        isEditable
+                                        //isLoading={postsLoading === 'loaded'}
+                                    />
+                                ))}
+                            </Grid>
+
                             <Grid xs={4} item>
                                 <TagsBlock
                                     items={["bestseller", "fun", "historic"]}
